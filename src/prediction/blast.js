6c9c3f9dd2a98116ae2b6912164f205652167667
navduhan <duhan27dec@gmail.com>
2022-11-17 13:59:14 -0700

    added blast

--- src/prediction/blast.js
+++ src/prediction/blast.js
@@ -11,7 +11,7 @@ const runBlast = (id, filedata, program, genome, word, target, evalue) => {
 
     console.log(filedata)
 
-    const child = require('child_process').execSync(`"/opt/software/ncbi-blast-2.7.1+-src/c++/bin/"+${program} -db ${path.join(__dirname, "../data/"+genome)} -query ${filedata} -max_target_seqs ${target} -word_size ${word} -evalue ${evalue} -outfmt 6 -out ${path.join(__dirname,`preddata/pred${id}.out.xml`)}`)
+    const child = require('child_process').execSync(`"/opt/software/ncbi-blast-2.7.1+-src/c++/bin/"+${program} -db ${path.join(__dirname, "../data/"+genome)} -query ${filedata} -max_target_seqs ${target} -word_size ${word} -evalue ${evalue} -num_threads 20 -outfmt 6 -out ${path.join(__dirname,`preddata/pred${id}.out.xml`)}`)
        
     return new Promise((res, rej) => {
        
